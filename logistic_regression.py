093c1f3c5e1ac1aa5536ced6ce26c1847fd27377
zAle711 <alessandro7111@gmail.com>
2023-05-30 18:02:02 +0200

    ADMM funziona tranne per la derivata del LAgrangiano Aumentato

--- logistic_regression.py
+++ logistic_regression.py
@@ -6,12 +6,24 @@ epsilon = 0.00001
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
-def cost_function(x, y, pesi_parametri):
-    h = sigmoid(np.matmul(x, pesi_parametri))
-    m = x.shape[0]
-    # h = sigmoid(x @ pesi_parametri)
-    cost = (np.matmul(-y.T, np.log(h + epsilon)) - np.matmul((1 -y.T), np.log(1 - h) + epsilon))/ m
-    return cost
+def cost_function(x, y, pesi_parametri, n_agenti=0):
+    
+    if n_agenti == 0:
+        h = sigmoid(np.matmul(x, pesi_parametri))
+        m = x.shape[0]
+        # h = sigmoid(x @ pesi_parametri)
+        cost = (np.matmul(-y.T, np.log(h + epsilon)) - np.matmul((1 -y.T), np.log(1 - h) + epsilon))/ m
+        return cost
+        
+    cost = np.zeros([n_agenti,1])
+    for i in range(0, n_agenti):
+        dati_per_agente = int(x.shape[0]/n_agenti)
+        x_i = x[dati_per_agente*i:dati_per_agente*(i+1), :]
+        y_i = y[dati_per_agente*i:dati_per_agente*(i+1)]
+
+        cost[i,:] = cost_function(x_i, y_i, pesi_parametri[:,i])
+    return cost.reshape(4)
+
 
 #Funzione che calcola l'Hessiana
 def compute_hessian(x, theta):
--- prove_2_ADMM.py
+++ prove_2_ADMM.py
@@ -5,34 +5,37 @@ from multiprocessing import Pool
 from sklearn.preprocessing import StandardScaler
 from logistic_regression import cost_function, sigmoid
 
+n_iterazioni = 1000
 n_agenti = 4
-n_iterazioni = 100
 n_parametri = 9
 
-u = np.zeros([n_parametri, n_iterazioni, n_agenti])
-z = np.zeros([n_parametri, n_iterazioni])
+# u = np.zeros([n_parametri, n_iterazioni, n_agenti])
 
-def update_local_variable(x_tr, y_tr):
-    pippo = 1
-    soluzioni = np.zeros([x_tr.shape[1], n_agenti])
+def update_local_variable(x_tr, y_tr, pesi_parametri, z, u, rho):
+    """_summary_
+    Args:
+        pesi_parametri : n_parametri X n_agenti
+        z : n_parametri
+        u : n_parametri X n_agenti
+    """
+
     dati_per_agente = int(x_tr.shape[0]/n_agenti)
+    
     for i in range(0, n_agenti):
-        print(f"Agente: {i}, range dati assegnato all'agente: {(dati_per_agente*i,dati_per_agente*(i+1))}")
+        #print(f"Agente: {i}, range dati assegnato all'agente: {(dati_per_agente*i,dati_per_agente*(i+1))}")
         x_i = x_tr[dati_per_agente*i:dati_per_agente*(i+1), :]
         y_i = y_tr[dati_per_agente*i:dati_per_agente*(i+1)]
 
         n_osservazioni = x_i.shape[0]
 
-        rho = 1
-        u = np.zeros([n_parametri, n_iterazioni])
-        u[:,0] = np.random.randn(n_parametri)
-        pesi_parametri = np.zeros([n_parametri, n_iterazioni])
-        pesi_parametri[:,0] = np.ones(n_parametri)
+        pesi_parametri_gradiente = np.zeros([n_parametri, n_iterazioni])
+        pesi_parametri_gradiente[:, 0] = pesi_parametri[:,i]
+        # print(pesi_parametri_gradiente.shape)
         valore_funzione_costo = []
-        valore_funzione_costo.append(cost_function(x_tr, y_tr, pesi_parametri[:,0]))  
+        valore_funzione_costo.append(cost_function(x_tr, y_tr, pesi_parametri[:, i]))
 
         # gradient = np.dot(x_tr.T, (h-y_tr)) / n_osservazioni
-        print(pesi_parametri[:, 0].shape)
+        #print(pesi_parametri[:, 0].shape)
         for j in range(0, n_iterazioni-1):
 
             if False:
@@ -42,16 +45,76 @@ def update_local_variable(x_tr, y_tr):
                 print("w: ", pesi_parametri[:,0].shape)
                 print("x_tr.T: ", x_i.T.shape, " y_tr: ", y_i.shape)
 
-            s = sigmoid(np.dot(x_i,pesi_parametri[:,0]))
+            s = sigmoid(np.dot(x_i,pesi_parametri[:,i]))
             #sicuramente z è quello dell'iterazione generale
-            pesi_parametri[:, j+1] = pesi_parametri[:, j] - (0.01) * (1/n_osservazioni) * (np.dot(x_i.T, (s - y_i))) + rho *  (pesi_parametri[:, j] - z[:,j] - u[:,j] )
+            pesi_parametri_gradiente[:, j+1] = pesi_parametri_gradiente[:, j]  - (0.01) * (1/x_i.shape[0]) * (np.dot(x_i.T, (s - y_i))) #+ rho *  (pesi_parametri_gradiente[:, j] - z - u[:,j] )
+        pesi_parametri[:, i] = pesi_parametri_gradiente[:,-1]
+    return pesi_parametri
+
 # Step-Size: 0.01 Costo: 0.5295995155297352
 # s:  (60390,)
 # x_tr.T:  (9, 60390)  y_tr:  (60390,)
 # G:  (9,)
 # w:  (9,)
 
+def update_g(pesi, u):
+    """
+    Args:
+        pesi : n_parametri X n_agenti
+        u : n_parametri X n_agenti
+    """
+    # print(pesi[:,1].shape)
+    z = np.zeros([n_parametri,1])
+    #Per calcolarla ho bisogno della media dei pesi sommata alla media di u
+    media_pesi = np.zeros(n_parametri)
+    media_u = np.zeros(n_parametri)
 
+    for i in range(0, n_agenti):
+        media_pesi += pesi[:,i] 
+        media_u += u[:,i]
+    
+    return (media_pesi + media_u) / n_agenti
+
+def update_u(u, pesi_parametri, z):
+    """
+    Args:
+        u: n_parametri x n_agenti
+        pesi_parametri : n_parametri x n_agenti
+        z : n_parametri
+    """
+
+    new_u = np.zeros([n_parametri, n_agenti])
+    
+
+    for i in range(n_agenti):
+        new_u[:,i] = u[:,i] + pesi_parametri[:,i] - z
+    
+    return new_u
+
+def ADMM(x_tr, y_tr):
+    #Iniziallizzo le matrici dei pesi, la variaible globale z e la variabile u
+    pesi_parametri = np.zeros([n_parametri, n_iterazioni, n_agenti])
+    z = np.zeros([n_parametri, n_iterazioni])
+    u = np.zeros([n_parametri, n_iterazioni, n_agenti])
+    #Inizializzo casualmente i primi pesi e valori di u
+    pesi_parametri[:,0,:] = np.random.randn(n_parametri,n_agenti)
+    u[:,0,:] = np.random.randn(n_parametri,n_agenti)
+    #Aggiorno la variaible globale con i nuovi valori
+    z[:,0] = update_g(pesi_parametri[:,0,:], u[:,0,:])
+    #Inizializzo matrice per raccogliere i valori della funzione costo
+    costo_logistic = np.zeros([n_agenti, n_iterazioni])
+    costo_logistic[:,0] = cost_function(x_tr, y_tr, pesi_parametri[:,0,:])
+
+    rho = 10
+
+    for i in range(0,10):
+        pesi_parametri[:,i+1,:] = update_local_variable(x_tr, y_tr, pesi_parametri[:,i,:], z[:,i], u[:,i,:], rho )
+        z[:, i+1] = update_g(pesi_parametri[:,i+1,:], u[:,i,:])
+        u[:,i+1,:] = update_u(u[:,i,:],pesi_parametri[:,i+1,:], z[:,i+1])
+        costo_logistic[:,i+1] = cost_function(x_tr, y_tr, pesi_parametri[:,i+1,:])
+    
+    print(pesi_parametri[:, 9, 0])
+        
 
 
 if __name__ == '__main__': 
@@ -66,4 +129,6 @@ if __name__ == '__main__':
     # Aggiungo il bias
     x_tr = np.hstack([np.ones([x_tr.shape[0],1]), x_tr])
     # Chiamata alla funzione ADMM per addestrare il modello logistic regression
-    update_local_variable(x_tr, y_tr)
+    #update_local_variable(x_tr, y_tr)
+
+    ADMM(x_tr, y_tr)
